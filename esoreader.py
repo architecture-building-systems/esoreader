e867710484a605c341febb3e3c74b9d2f0680ed5
daren-thomas <dthomas.ch@gmail.com>
2015-04-21 16:38:15 +0200

    moved some stuff around (refactoring)

--- esoreader.py
+++ esoreader.py
@@ -43,6 +43,19 @@ Example
 '''
 
 
+def read(eso_file_path):
+    """Read in an .eso file and return the data dictionary and a dictionary
+    representing the data.
+    This is probably the function you are looking for...
+    """
+    eso_file = open(eso_file_path, 'r')
+    parser = EsoParser(eso_file)
+    dd = parser.read_data_dictionary()
+    dd.build_index()
+    data = parser.read_data(dd)
+    return dd, data
+
+
 class DataDictionary(object):
     def __init__(self, version=None, timestamp=None):
         '''
@@ -72,80 +85,72 @@ class DataDictionary(object):
                 if search.lower() in variable_name.lower()]
 
 
-def read_reporting_frequency(line):
-    reporting_frequency = None
-    if '! ' in line:
-        line = line.split('! ')[0]
-    if ' !' in line:
-        line, reporting_frequency = line.split(' !')
-        # RunPeriod contains more stuff (" [Value,Min,Month,Day,Hour,Minute,
-        # Max,Month,Day,Hour,Minute]")split it off
-        reporting_frequency = reporting_frequency.split()[0]
-    return line, reporting_frequency
+class EsoParser(object):
 
+    def __init__(self, eso_file):
+        self.eso_file = eso_file
 
-def read_variable_unit(variable):
-    unit = None
-    if '[' in variable:
-        variable, unit = variable.split('[')
-        unit = unit[:-1]  # remove ']' at the end
-        variable = variable.strip()
-    return variable, unit
+    def read_reporting_frequency(self, line):
+        reporting_frequency = None
+        if '! ' in line:
+            line = line.split('! ')[0]
+        if ' !' in line:
+            line, reporting_frequency = line.split(' !')
+            # RunPeriod contains more stuff (" [Value,Min,Month,Day,Hour,
+            # Minute, Max,Month,Day,Hour,Minute]")split it off
+            reporting_frequency = reporting_frequency.split()[0]
+        return line, reporting_frequency
 
+    def read_variable_unit(self, variable):
+        unit = None
+        if '[' in variable:
+            variable, unit = variable.split('[')
+            unit = unit[:-1]  # remove ']' at the end
+            variable = variable.strip()
+        return variable, unit
 
-def read(eso_file_path):
-    """Read in an .eso file and return the data dictionary and a dictionary
-    representing the data.
-    This is probably the function you are looking for...
-    """
-    eso_file = open(eso_file_path, 'r')
-    dd = _read_data_dictionary(eso_file)
-    dd.build_index()
-    data = _read_data(dd, eso_file)
-    return dd, data
-
-
-def _read_data_dictionary(eso_file):
-    """parses the head of the eso_file, returning the data dictionary.
-    the file object eso_file is advanced to the position needed by
-    _read_data.
-    """
-    version, timestamp = [s.strip() for s in eso_file.next().split(',')[-2:]]
-    dd = DataDictionary(version, timestamp)
-    line = eso_file.next().strip()
-    while line != 'End of Data Dictionary':
-        line, reporting_frequency = read_reporting_frequency(line)
-        if reporting_frequency:
-            fields = [f.strip() for f in line.split(',')]
-            if len(fields) >= 4:
-                id, nfields, key, variable = fields[:4]
+    def read_data_dictionary(self):
+        """parses the head of the eso_file, returning the data dictionary.
+        the file object eso_file is advanced to the position needed by
+        read_data.
+        """
+        version, timestamp = [s.strip() for s
+                              in self.eso_file.next().split(',')[-2:]]
+        dd = DataDictionary(version, timestamp)
+        line = self.eso_file.next().strip()
+        while line != 'End of Data Dictionary':
+            line, reporting_frequency = self.read_reporting_frequency(line)
+            if reporting_frequency:
+                fields = [f.strip() for f in line.split(',')]
+                if len(fields) >= 4:
+                    id, nfields, key, variable = fields[:4]
+                else:
+                    id, nfields, variable = fields[:3]
+                    key = None
+                variable, unit = self.read_variable_unit(variable)
+                dd.variables[int(id)] = [reporting_frequency, key,
+                                         variable, unit]
             else:
-                id, nfields, variable = fields[:3]
-                key = None
-            variable, unit = read_variable_unit(variable)
-            dd.variables[int(id)] = [reporting_frequency, key, variable, unit]
-        else:
-            # ignore the lines that aren't report variables
-            pass
-        line = eso_file.next().strip()
-    dd.ids = set(dd.variables.keys())
-    return dd
+                # ignore the lines that aren't report variables
+                pass
+            line = self.eso_file.next().strip()
+        dd.ids = set(dd.variables.keys())
+        return dd
 
-
-def _read_data(dd, eso_file):
-    '''parse the data from the .eso file returning,
-    NOTE: eso_file should be the same file object that was passed to
-    _read_data_dictionary(eso_file) to obtain dd.'''
-    data = {}  # id => [value]
-    for id in dd.variables.keys():
-        data[id] = []
-    for line in eso_file:
-        if line.startswith('End of Data'):
-            break
-        fields = [f.strip() for f in line.split(',')]
-        id = int(fields[0])
-        if id not in dd.ids:
-            # skip entries that are not output:variables
-            continue
-        data[id].append(float(fields[1]))
-    return data
+    def read_data(self, dd):
+        '''parse the data from the .eso file returning,
+        NOTE: eso_file should be the same file object that was passed to
+        read_data_dictionary(eso_file) to obtain dd.'''
+        data = {}  # id => [value]
+        for id in dd.variables.keys():
+            data[id] = []
+        for line in self.eso_file:
+            if line.startswith('End of Data'):
+                break
+            fields = [f.strip() for f in line.split(',')]
+            id = int(fields[0])
+            if id not in dd.ids:
+                # skip entries that are not output:variables
+                continue
+            data[id].append(float(fields[1]))
+        return data
