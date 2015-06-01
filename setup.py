d2ab9c562bece298d01a2dd6afb5acf7ddd632f8
daren-thomas <dthomas.ch@gmail.com>
2015-06-01 16:14:10 +0200

    finished documenting v1.1

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
+import os
 
-long_description = open('README.rst', 'r').read()
+
+long_description='see the GitHub repository for more information: https://github.com/architecture-building-systems/esoreader'
+if os.path.exists('README.rst'):
+    long_description = open('README.rst').read()
+
 
 setup(
     name='esoreader',
     py_modules=['esoreader'],  # this must be the same as the name above
     version='1.1.0',
     description='A module for parsing EnergyPlus *.eso files',
-    #long_description=long_description,
+    long_description=long_description,
     author='Daren Thomas',
-    author_email='dthomas.ch@gmail.com',
+    author_email='thomas@arch.ethz.ch',
     url='https://github.com/architecture-building-systems/esoreader',
     download_url='https://github.com/architecture-building-systems/esoreader/tarball/1.1',  # noqa
     keywords=['simulation', 'parsing', 'energyplus', 'pandas'],  # arbitrary keywords  # noqa
